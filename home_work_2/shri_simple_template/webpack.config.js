7d2e95504957eeca38614eb77d9e00b917220624
Dinarhak <jjustdinar@gmail.com>
2022-10-28 21:46:31 +0300

    Именил package.json и конфиги webpack

--- home_work_2/shri_simple_template/webpack.config.js
+++ home_work_2/shri_simple_template/webpack.config.js
@@ -21,6 +21,16 @@ const config = {
     },
     module: {
         rules: [
+        {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/env', '@babel/preset-react'] },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
             // @TODO js rule
             // @TODO css rule
         ],
